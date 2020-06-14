6a7835f11e4a0bb4573b374c3d5de695fb676ba1
wind <mid_autumn@126.com>
2020-06-14 17:39:07 +0800

    x

--- output/type1.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Date: 2020-6-12 13:53:37
- * @LastEditors: Huang canfeng
- * @LastEditTime: 2020-6-12 13:53:37
- * @Description:
- */
-import { IResponseType } from "@/utils/fetch/type"; //---------------------支持亿联,财务公司(薪宝和云企蕙),支付宝的提现,根据不同业务处理不同----------------------
