c9689bcca21137af135f71b3a5dd72fa13191d33
wind <mid_autumn@126.com>
2020-07-17 14:41:50 +0800

    x

---
+++ config/tsdoc-config.js
@@ -0,0 +1,16 @@
+/*
+ * @Date: 2020-06-14 18:16:58
+ * @LastEditors: Huang canfeng
+ * @LastEditTime: 2020-06-17 20:19:37
+ * @Description:
+ */
+
+module.exports = {
+	author: "Huang canfeng",
+	username: "13689595749",
+	password: "123456",
+	fetchFnName: "{ get, post, postJson }",
+	fetchfilePath: "@/utils/fetch/index",
+	responseFnName: "{ IResponseType }",
+	responsefilePath: "@/utils/fetch/type",
+};
